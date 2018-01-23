9f26e0a400355eaf9126a1f3de13779c4b513e03
Rowland Ekemezie <mentrie@users.noreply.github.com>
2018-01-23 12:30:41 +0100

    [chore #1]: Bootstrap project (#3)
    * [chore #1]:
    - Setup testing infrastructure
    - Setup up CI/CD pipeline
    - Setup project directories
    - Enable CSS preprocessor usage
    
    * chore[#1]: - Add strict eslint rules
    - Fix eslint issues with files

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-  
+
 registerServiceWorker();
