9fd2fb5e1fae298b6cb9c49a3ce43253b6d9a09f
Rowland Ekemezie <mentrie@users.noreply.github.com>
2018-01-24 15:07:59 +0100

    [Feat #9]: Add search functionality (#11)
    * - Add search functionality
    - Add unit tests for components
    - Test util functions
    
    * [feat #6]: Specify heroku app to deploy to

--- src/containers/Main/PokemonList.js
+++ src/containers/Main/PokemonList.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PokemonItem from './PokemonItem';
 import Link from 'react-router-dom/Link';
-import { getPokemanId } from '../../helpers/utils';
+import { getPokemonId } from '../../helpers/utils';
 
 const PokemonList = ({ pokemons, onSelect }) => {
   const renderPokemons = () => {
     return pokemons.map((pokemon, index) => (
       <Link
-        to={`/${pokemon.name}/${getPokemanId(pokemon.url)}`}
+        to={`/${pokemon.name}/${getPokemonId(pokemon.url)}`}
         key={index}
         className="pokeman__list__item pokemon__no__link"
         onClick={onSelect}
